833b365ad18c5c45e39415e678c41ac95e17f666
hemorgenes <kauan7622@gmail.com>
2022-11-07 13:57:07 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
